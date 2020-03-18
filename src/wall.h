33108e0c1b5f9fd29acc689ae3c2504841ca0e34
Brenden Singh <bsingh@pop-os.localdomain>
2020-03-17 20:08:36 -0700

    Walls working

---
+++ src/wall.h
@@ -0,0 +1,44 @@
+
+#ifndef __WALL_AGENT__H
+#define __WALL_AGENT__H
+
+#include "enviro.h"
+#include <stdio.h>
+
+using namespace enviro;
+
+class WallController : public Process, public AgentInterface
+{
+
+public:
+    WallController() : Process(), AgentInterface() {}
+
+    void init()
+    {
+        prevent_rotation();
+    }
+    void start() {}
+
+    void update()
+    {
+        //omni_apply_force(0, 500);
+    }
+
+    void stop() {}
+};
+
+class Wall : public Agent
+{
+public:
+    Wall(json spec, World &world) : Agent(spec, world)
+    {
+        add_process(c);
+    }
+
+private:
+    WallController c;
+};
+
+DECLARE_INTERFACE(Wall)
+
+#endif
